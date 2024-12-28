94cdefa549893e4280447db29e090561280c2b70
Jun Yu (RTX) <shockzinfinity@gmail.com>
2024-12-28 19:13:28 +0900

    feat: normalize file, directory

--- PathPolisher/Program.cs
+++ PathPolisher/Program.cs
@@ -1,2 +1,127 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+internal class Program
+{
+  private static async Task<int> Main(string[] args)
+  {
+    var rootCommand = new RootCommand("파일/디렉토리 이름 자음모음분리 현상 해결을 위한 툴");
+
+    var directoryParameter = new Option<DirectoryInfo>(
+      aliases: new[] { "--directory", "-d" },
+      //getDefaultValue: () => new DirectoryInfo(Environment.CurrentDirectory),
+      getDefaultValue: () => new DirectoryInfo("c:/Users/shockz/TeamEPS/00.DSN.Solution"),
+      description: "Target directory"
+      );
+
+    rootCommand.AddOption(directoryParameter);
+
+    rootCommand.SetHandler((directoryInfo) =>
+    {
+      try
+      {
+        if (directoryInfo is null || !directoryInfo.Exists)
+        {
+          Console.WriteLine($"오류: 지정된 디렉터리 '{directoryInfo?.FullName}'는 존재하지 않습니다.");
+          return;
+        }
+
+        Console.WriteLine($"대상 디렉토리: '{directoryInfo.FullName}'");
+        NormalizeDirectory(directoryInfo.FullName);
+      }
+      catch (DirectoryNotFoundException ex)
+      {
+        Console.WriteLine($"대상 디렉터리를 확인할 수 없음: {ex.Message}");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"알 수 없는 오류 발생: {ex.Message}");
+      }
+    }, directoryParameter);
+
+    return await rootCommand.InvokeAsync(args);
+  }
+
+  private static void NormalizeDirectory(string path)
+  {
+    Parallel.ForEach(Directory.GetFiles(path), HandleFileNormalization);
+
+    Parallel.ForEach(Directory.GetDirectories(path), HandleDirectoryNormalization);
+  }
+
+  private static void HandleFileNormalization(string file)
+  {
+    try
+    {
+      string filename = Path.GetFileName(file);
+      if (!IsNormalized(filename) && !IsSystemOrHiddenFile(file))
+      {
+        string normalizedFileName = filename.Normalize();
+        string normalizedPath = Path.Combine(Path.GetDirectoryName(file)!, normalizedFileName);
+
+        if (!File.Exists(normalizedPath) && !file.Equals(normalizedPath, StringComparison.OrdinalIgnoreCase))
+        {
+          File.Move(file, normalizedPath);
+          Console.WriteLine($"파일 정규화: {filename} -> {normalizedFileName}");
+        }
+      }
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"파일 정규화 중 오류 발생: {ex.Message}");
+    }
+  }
+
+  private static void HandleDirectoryNormalization(string dir)
+  {
+    try
+    {
+      string dirName = Path.GetFileName(dir);
+      string parentPath = Path.GetDirectoryName(dir)!;
+
+      if (!IsNormalized(dirName) && !IsSystemOrHiddenDirectory(dir))
+      {
+        string normalizedDirName = dirName.Normalize();
+        string normalizedDirPath = Path.Combine(parentPath, normalizedDirName);
+
+        if (!Directory.Exists(normalizedDirPath) && !dir.Equals(normalizedDirPath, StringComparison.OrdinalIgnoreCase))
+        {
+          Directory.Move(dir, normalizedDirPath);
+          Console.WriteLine($"디렉토리 정규화: {dirName} -> {normalizedDirName}");
+
+          NormalizeDirectory(normalizedDirPath);
+        }
+        else
+        {
+          NormalizeDirectory(dir);
+        }
+      }
+      else
+      {
+        NormalizeDirectory(dir);
+      }
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"디렉토리 정규화 중 오류 발생: {ex.Message}");
+    }
+  }
+
+  private static bool IsNormalized(string input)
+  {
+    return input.IsNormalized();
+  }
+
+  private static bool IsSystemOrHiddenFile(string path)
+  {
+    var attributes = File.GetAttributes(path);
+    return (attributes & FileAttributes.Hidden) == FileAttributes.Hidden ||
+      (attributes & FileAttributes.System) == FileAttributes.System;
+  }
+
+  private static bool IsSystemOrHiddenDirectory(string path)
+  {
+    var attributes = File.GetAttributes(path);
+    return (attributes & FileAttributes.Hidden) == FileAttributes.Hidden ||
+      (attributes & FileAttributes.System) == FileAttributes.System;
+  }
+}
